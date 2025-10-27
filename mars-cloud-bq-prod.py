e440314350078d4045c6444bb73c937dca1408be
Venki124 <venkat.venki124@gmail.com>
2025-10-27 20:49:57 +0530

    update code

--- mars-cloud-bq-prod.py
+++ mars-cloud-bq-prod.py
@@ -52,6 +52,7 @@ def run():
         '--service_account_email='+project_number+'-compute@developer.gserviceaccount.com',
         '--save_main_session',
         '--experiments=use_runner_v2',
+        '--experiments=enable_streaming_engine',
         '--experiments=enable_bq_streaming_insert_v2'
     ]
 
@@ -77,8 +78,9 @@ def run():
         | 'write to BQ' >> beam.io.WriteToBigQuery(table=output_table,write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
                                                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                                                    schema='timestamp:STRING, ipaddr:STRING, action:STRING, srcacct:STRING, destacct:STRING, amount:FLOAT, customername:STRING',
-                                                #    method=beam.io.WriteToBigQuery.Method.STREAMING_INSERTS
-                                                   method=beam.io.WriteToBigQuery.Method.FILE_LOADS)
+                                                   method=beam.io.WriteToBigQuery.Method.STREAMING_INSERTS
+                                                #    method=beam.io.WriteToBigQuery.Method.FILE_LOADS
+                                                )
     )
 
     p.run().wait_until_finish()
