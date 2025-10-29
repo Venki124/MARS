6442a18ebecd1e6dcf7d1a0e3c86e7de8d089d6a
Venki124 <venkat.venki124@gmail.com>
2025-10-30 00:07:06 +0530

    updated code

--- streaming/prep-project-stream.sh
+++ streaming/prep-project-stream.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # MAKE SURE GCP PROJECT IS SET
 # gcloud config set project PROJECT_ID
+
+# set the project
+gcloud config set project $(gcloud projects list --format='value(PROJECT_ID)' | grep playground)
+
 if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
     echo "Project has not been set! Please run:"
     echo "   gcloud config set project PROJECT_ID"
@@ -8,8 +12,10 @@ if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
 else
     echo "Project Name: $GOOGLE_CLOUD_PROJECT"
     gcloud storage buckets create gs://$GOOGLE_CLOUD_PROJECT"-bucket" --soft-delete-duration=0
-    gcloud services disable dataflow.googleapis.com --force
+    # gcloud services disable dataflow.googleapis.com --force
     gcloud services enable dataflow.googleapis.com
+
+<<COMMENT
     gcloud iam service-accounts create marssa
     sleep 1
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/editor
@@ -17,9 +23,13 @@ else
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/dataflow.worker
     sleep 1
     gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member user:$USER_EMAIL --role roles/iam.serviceAccountUser
+COMMENT
+    echo "Creating the mars dataset"
     bq mk mars
-    bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
-    bq mk --schema message:STRING -t mars.raw
+
+    # bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
+    # bq mk --schema message:STRING -t mars.raw
+    echo "Creating pubsub topic and subscription
     gcloud pubsub topics create activities-topic
     gcloud pubsub subscriptions create activities-subscription --topic activities-topic
 fi
